b9f0327decf2ac5c86d272114b543197b0fa69d8
pixel1401 <pixel1401@gmail.com>
2022-03-29 08:16:44 +0500

    4 lesson

---
+++ src/containers/App.jsx
@@ -0,0 +1,14 @@
+import cn from 'classnames'
+import s from './App.module.css';
+
+
+function App() {
+  return (
+    <div>
+      <h1 className='header'>Hello</h1>
+      <h2 className={cn(s.header , s.text)}>erzhna</h2>
+    </div>
+  );
+}
+
+export default App;
